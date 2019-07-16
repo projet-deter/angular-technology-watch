94f567946506278eb2d8fdf5d88088900ea650ab
Leyla LE NOAN <leyla.lenoan@outlook.fr>
2019-07-16 17:11:22 +0200

    feat/global-style - Add default and not-found routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ArticlesComponent } from './components/articles/articles.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 
 const routes: Routes = [
   { path: 'articles', component: ArticlesComponent },
+  { path: '', redirectTo: '/articles', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
 import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ApiService } from './services/api.service';
 import { ArticlesComponent } from './components/articles/articles.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ArticlesComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
