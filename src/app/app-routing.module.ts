27adae8925840569cbf616a8400ef5aaf459884b
Leyla LE NOAN <leyla.lenoan@outlook.fr>
2019-07-17 16:49:01 +0200

    feat/global-style - Update global-style + global routing + add colors scss files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { ArticlesComponent } from './components/articles/articles.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 const routes: Routes = [
-  { path: 'articles', component: ArticlesComponent },
-  { path: '', redirectTo: '/articles', pathMatch: 'full' },
+  { path: '',
+    component: HomeComponent,
+    children: [
+      { path: '', redirectTo: '/articles', pathMatch: 'full' },    
+      { path: 'articles', component: ArticlesComponent },
+    ]
+  },
+  // { path: 'profile', component: ProfileComponent },
+  // { path: 'login', component: LoginComponent },
+  // { path: 'register', component: RegisterComponent },
+  // { path: 'logout', xx },
   { path: '**', component: NotFoundComponent }
 
 ];
